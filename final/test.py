a8c76c702d3211a659d99db9026de62f9167cbbb
Jenny <jennysunnjm@gmail.com>
2024-04-27 09:32:59 -0400

    Tower sorted

--- final/test.py
+++ final/test.py
@@ -1,7 +1,7 @@
 import json
 
 # read in simpletower.json and sort as json
-with open("simpletower.json") as json_file:
+with open("json/tower-x-pos-y-neg.json") as json_file:
     tower_json = json.load(json_file)
 
 # convert tower_json to list of objects
@@ -13,14 +13,14 @@ for key in tower_json.keys():
     # print(tower_objects)
 
 # sort list of objects by their z value
-tower_objects.sort(key=lambda x: x["position"][2])
+tower_objects.sort(key=lambda x: x["position"][0])
 
 # write sorted list of objects to simpletower_sorted.json
-with open("simpletower_sorted.json", "w") as json_file:
+with open("json/tower.json", "w") as json_file:
     json.dump(tower_objects, json_file, indent=4)
 
 # read in simpletower.json which is a list of dicts
-with open("simpletower_sorted.json") as json_file:
+with open("json/tower.json") as json_file:
     tower_json = json.load(json_file)
 
 print("tower_json is", tower_json)
